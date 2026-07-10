6961eaf4668354a5ea31429a0d68ee9bcd851191
as9n1n3 <mashmas2003@gmail.com>
2026-07-10 06:59:47 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -25,15 +25,21 @@ class Opinion(db.Model):
     title = db.Column(db.String(128), nullable=False)
     text = db.Column(db.Text, unique=True, nullable=False)
     source = db.Column(db.String(256))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    timestamp = db.Column(
+        db.DateTime,
+        index=True,
+        default=datetime.utcnow
+    )
     added_by = db.Column(db.String(64))
 
 
 class OpinionForm(FlaskForm):
     title = StringField(
         'Введите название фильма',
-        validators=[DataRequired(message='Обязательное поле'),
-                    Length(1, 128)]
+        validators=[
+            DataRequired(message='Обязательное поле'),
+            Length(1, 128)
+        ]
     )
     text = TextAreaField(
         'Напишите мнение',
@@ -41,7 +47,10 @@ class OpinionForm(FlaskForm):
     )
     source = URLField(
         'Добавьте ссылку на подробный обзор фильма',
-        validators=[Length(1, 256), Optional()]
+        validators=[
+            Length(1, 256),
+            Optional()
+        ]
     )
     submit = SubmitField('Добавить')
 
